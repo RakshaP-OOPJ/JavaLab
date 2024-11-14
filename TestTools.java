51af7f20627a039dd8544607796994cac308b306
RakshaP-OOPJ <raksha.23cs112@sode-edu.in>
2024-11-14 10:35:43 +0530

    Update TestTools.java

--- TestTools.java
+++ TestTools.java
@@ -2,17 +2,11 @@ package pkg1;
 public class TestTools {
     public static void main(String[] args){
         Tools t1=new Tools();
-        System.out.println("numbers(10):");
         t1.numbers(10);
-        System.out.println("even(20):");
         t1.even(10);
-        System.out.println("odd(20):");
         t1.odd(10);
-        System.out.println("mult3:");
         t1.mult3(10);
-        System.out.println("mult4:");
         t1.mult4(10);
-        System.out.println("mult5:");
         t1.mult5(10);
     }
 }
