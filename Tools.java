7e4ff019e11680c6245ef1c43f2f5ed7c125a8e6
RakshaP-OOPJ <raksha.23cs112@sode-edu.in>
2024-11-14 10:13:19 +0530

    Add files via upload

---
+++ Tools.java
@@ -0,0 +1,42 @@
+package pkg1;
+public class Tools {
+    public void numbers(int n1) {
+        for(int i=0;i<=n1;i++){
+            System.out.print(i+" ");
+        }    
+            System.out.println();
+        }
+    public void even(int n){
+        for(int i=0;i<=n;i+=2){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+     public void odd(int n){
+        for(int i=1;i<=n;i+=2){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+      public void mult3(int n){
+        for(int i=0;i<=n;i+=3)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+     public void mult4(int n){
+        for(int i=0;i<=n;i+=4){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+      public void mult5(int n){
+        for(int i=0;i<=n;i+=5){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
+
+    
